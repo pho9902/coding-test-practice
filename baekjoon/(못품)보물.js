922f235c0049747a7041dd80cb404e2c4aa34d75
unknown <pho9902@naver.com>
2022-03-29 10:50:10 +0900

    IOIOI 풀이

---
+++ baekjoon/(못품)보물.js
@@ -0,0 +1,14 @@
+// // var fs = require("fs");
+// // var input = fs.readFileSync("/dev/stdin").toString().split("\n");
+// const input = "5\n1 1 1 6 0\n2 7 8 3 1".split("\n");
+
+// const A = input[1].split(" ");
+// const B = input[2].split(" ");
+
+// const sortedB = [...B].sort((a, b) => a - b);
+// const obj = {};
+// sortedB.forEach((el, idx) => {
+//   if (obj[el] === undefined) obj[el] = idx;
+// });
+
+// console.log(obj);
